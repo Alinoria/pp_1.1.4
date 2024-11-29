b5a3edfd77aadcdce6481b5da9b16da60e6178c4
alinoria <alinoria.expecto@gmail.com>
2024-11-29 16:50:41 +0300

    add util and daoJDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    static final String URL = "jdbc:mysql://localhost:3306/my_db";
+    static final String USER = "root";
+    static final String PASSWORD = "jpacourse";
+
+    public static Connection getConnection() {
+        try{Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Успешное подключение!");
+            return connection;}
+
+        catch (SQLException e){
+            System.out.println("Не получилось подключиться");
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
